ce25dbbfa50af516c167e9cd4f6088f92ffe6fd2
Vilker <vilkersousa@outlook.com>
2026-01-27 14:59:25 -0300

    Atualizando espaços dos comentários

--- tipos/01tipos.go
+++ tipos/01tipos.go
@@ -5,13 +5,13 @@ import (
 )
 
 func main() {
-	//bool (true / false)
+	// bool (true / false)
 	// fmt.Printlf("type: %T - Value: %v\n", false, false)
 
-	//string - sequencia de bytes
+	// string - sequencia de bytes
 	fmt.Printf("Type: %T - Value: %v\n", "Vilker", "Vilker")
 
-	//fmt.Printf("Type: %T - Value: %v\n", 1, 1)
-	//fmt.Printf("Type: %T - Value: %v\n", "1", "1")
-	//fmt.Printf("Type: %T - Value: %v\n", 1.123, 1.123)
+	// fmt.Printf("Type: %T - Value: %v\n", 1, 1)
+	// fmt.Printf("Type: %T - Value: %v\n", "1", "1")
+	// fmt.Printf("Type: %T - Value: %v\n", 1.123, 1.123)
 }
