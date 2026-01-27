2d9f7e9b624377123f5f28a9c0618ed9809f336a
Vilker <vilkersousa@outlook.com>
2026-01-27 14:54:56 -0300

    Organizando exercícios em subpastas por aula

---
+++ tipos/01tipos.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//bool (true / false)
+	// fmt.Printlf("type: %T - Value: %v\n", false, false)
+
+	//string - sequencia de bytes
+	fmt.Printf("Type: %T - Value: %v\n", "Vilker", "Vilker")
+
+	//fmt.Printf("Type: %T - Value: %v\n", 1, 1)
+	//fmt.Printf("Type: %T - Value: %v\n", "1", "1")
+	//fmt.Printf("Type: %T - Value: %v\n", 1.123, 1.123)
+}
