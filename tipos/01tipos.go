697b6e91e676960b0e0375ad07bc88ea0e75ee44
Vilker <vilkersousa@outlook.com>
2026-01-27 15:08:56 -0300

    Atualizando os tipos

--- tipos/01tipos.go
+++ tipos/01tipos.go
@@ -8,10 +8,16 @@ func main() {
 	// bool (true / false)
 	// fmt.Printlf("type: %T - Value: %v\n", false, false)
 
-	// string - sequencia de bytes
+	// string - sequência de bytes
 	fmt.Printf("Type: %T - Value: %v\n", "Vilker", "Vilker")
 
 	// fmt.Printf("Type: %T - Value: %v\n", 1, 1)
 	// fmt.Printf("Type: %T - Value: %v\n", "1", "1")
 	// fmt.Printf("Type: %T - Value: %v\n", 1.123, 1.123)
 }
+
+// Tipos:
+// bool (true / false)
+// string - sequência de bytes
+// int - números inteiros
+// float (float64, float32) - para números decimais
