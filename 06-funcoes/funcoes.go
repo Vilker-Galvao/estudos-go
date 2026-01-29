621e7f1d5d56f4d8de98371667b2f9d1fc4f743d
Vilker <vilkersousa@outlook.com>
2026-01-29 10:10:56 -0300

    Organizando exercícios com numeração de pastas

---
+++ 06-funcoes/funcoes.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(soma(42, 13))
+	
+	SomaDosValores := soma (10, 13)      // funciona do mesmo jeito
+	fmt.Println(SomaDosValores)
+
+	sub := subtracao(10, 5)
+	fmt.Println(sub)
+
+	nome, sobrenome := printNomeCompleto("VILKER", "GALVAO")
+	fmt.Println(nome)
+	fmt.Println(sobrenome)
+}
+
+// Função começando com letra minúscula:
+// Função é PRIVADA
+// Função ela só pode ser utilizada no próprio pacote
+func printNomeCompleto(nome, sobrenome string) (string, string) {
+	return nome, sobrenome
+}
+
+// Função começando com letra maiúscula:
+// Função é PÚBLICA
+// Função pode ser utilizada fora do próprio pacote
+// Como utilizaria ela fora: main.PrintNome(nome)
+func PrintNome(nome string) string {
+	return nome
+}
+
+
+
+func subtracao (x int, y int) int {
+	return x - y
+}
+
+func soma (x int, y int) int {
+	return x + y
+}
